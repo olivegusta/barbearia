132010442112b8ecc5b68dfa7c0805a0d3c1f37b
Oliveiragusta <oliveiragustavo801@gmail.com>
2021-10-11 21:10:23 -0300

    atualizaçao

--- barbearia/lib/main.dart
+++ barbearia/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyLogin(title: "Agendamento!!"),
+      home: MyHomePage(title: "Agendamento!!"),
     );
   }
 }
