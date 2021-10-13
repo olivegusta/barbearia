41473654af6200047f19f9ee13a0981a388c5f2b
Oliveiragusta <oliveiragustavo801@gmail.com>
2021-10-13 20:53:45 -0300

    atualizaçao

--- barbearia/lib/pages/home.dart
+++ barbearia/lib/pages/home.dart
@@ -23,25 +23,3 @@ class _homeState extends State<home> {
     );
   }
 }
-
-Widget bandeiraBeldica() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      faixa(cor: AppConsts.black),
-      faixa(cor: AppConsts.yellowBasic),
-      faixa(cor: AppConsts.redBasic)
-    ],
-  );
-}
-
-Widget faixa({cor}) {
-  final double h = (250.0);
-  final double l = (120.0);
-
-  return Container(
-    height: h,
-    width: l,
-    color: cor,
-  );
-}
